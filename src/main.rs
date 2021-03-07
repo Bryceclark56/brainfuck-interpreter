587da31ee35d148f16328ca9cdffd1f28221a420
Bryce Clark <bryce@lowquality.dev>
2021-03-07 01:02:59 -0600

    Commands represented as enum variants
    - enum for errors
    - conversion from char to Command
    - parse_string to convert string to Vec<Command>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{char, cmp::Ordering, io::{Write, stdout}, process::exit};
+use std::{char, convert::{TryFrom, TryInto}, error::Error, fmt::{self, write}, io::{Write, stdout}, process::exit};
 
 fn main() {
     let crate_version: &'static str = env!("CARGO_PKG_VERSION");
@@ -15,30 +15,7 @@ fn main() {
 
     println!("Input: {}", &buffer); // Debugging purposes
 
-    //Validate input
-    // Check for valid characters
-    const VALID_CHARS: &[char; 8] = &['>', '<', '+', '-', '.', ',', '[', ']'];
-
-    for c in buffer.chars() {
-        if VALID_CHARS.iter().any(|vc| char::cmp(&c, vc) == Ordering::Equal) {
-            eprintln!(" '{}' is not a valid Brainfuck command", &c);
-            exit(1); //TODO: Don't exit, just re-prompt
-        }
-    }
-
-    // Check for valid structure (Every '[' has a ']' )
-    let mut loop_stack = buffer.chars()
-        .filter(|c| ['[', ']'].contains(&c))
-        .collect::<Vec<char>>();
-
-    if !loop_stack.is_empty() {
-        if loop_stack.pop().unwrap() == '[' {
-            eprintln!("Missing closing loop bracket");
-        }
-        else {
-            eprintln!("Missing opening loop bracket");
-        }
-    }
+    let commands = Command::parse_string(buffer);
 
     // Execute input as brainfuck commands
 
@@ -46,3 +23,101 @@ fn main() {
 
     // Exit
 }
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Command {
+    IncrementPointer,
+    DecrementPointer,
+    //
+    Increment,
+    Decrement,
+    //
+    Output,
+    Input,
+    //
+    LoopStart,
+    LoopEnd,
+}
+
+impl Command {
+    fn parse_string(s: &str) -> Result<Vec<Command>, BrainfuckError> {
+        // Convert the characters into commands
+        let commands = s.chars()
+        .map(|c| Command::try_from(&c))
+        .collect::<Result<Vec<Command>, BrainfuckError>>()?;
+
+        // Check for matching square brackets
+        let mut stack: Vec<Command> = Vec::new();
+        for cmd in &commands {
+            match cmd {
+                Command::LoopStart => stack.push(*cmd),
+                Command::LoopEnd => {
+                    if *stack.last().unwrap_or_else(|| &&Command::Input) == Command::LoopStart {
+                        stack.pop();
+                    }
+
+                    stack.push(*cmd);
+                },
+                _ => {}
+            }
+        }
+
+        // I should say where in the input as well
+        if stack.len() > 0 {
+            let last_bracket = stack.last().unwrap_or_else(|| &&Command::Input);
+
+            return Err(BrainfuckError::MissingBracketError(
+                *last_bracket == Command::LoopEnd
+            ));
+        }
+
+
+        Ok(commands)
+    }
+}
+
+impl TryFrom<&char> for Command {
+    type Error = BrainfuckError;
+
+    fn try_from(c: &char) -> Result<Self, Self::Error> {
+        match c {
+            '>' => Ok(Command::IncrementPointer),
+            '<' => Ok(Command::DecrementPointer),
+    
+            '+' => Ok(Command::Increment),
+            '-' => Ok(Command::Decrement),
+    
+            '.' => Ok(Command::Output),
+            ',' => Ok(Command::Input),
+    
+            '[' => Ok(Command::LoopStart),
+            ']' => Ok(Command::LoopEnd),
+
+            _ => Err(BrainfuckError::InvalidCommandError(c.clone()))
+        }
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
+enum BrainfuckError {
+    InvalidCommandError(char),
+    MissingBracketError(bool)
+}
+
+impl fmt::Display for BrainfuckError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            BrainfuckError::InvalidCommandError(cmd) => write!(f, "Invalid command: {}", cmd),
+            BrainfuckError::MissingBracketError(is_closing_bracket) => {
+                if *is_closing_bracket {
+                    write!(f, "Missing closing bracket")
+                }
+                else {
+                    write!(f, "Missing opening bracket")
+                }
+            }
+        }
+    }
+}
+
+impl std::error::Error for BrainfuckError {}
